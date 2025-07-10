055b043b4d86c84bb71e3b16633febf62efc54bc
Iuli <iulianandrei7771@gmail.com>
2025-07-10 20:15:49 +0300

    adaugat login

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from flask_login import LoginManager
+from flask_login import UserMixin
+
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
 
     from app.routes import main
-    app.register_blueprint(main)  
+    app.register_blueprint(main)
 
     return app
