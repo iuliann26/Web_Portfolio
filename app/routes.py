0ee08357e7a94586a57fa876d76e28115ab6bd21
Iuli <iulianandrei7771@gmail.com>
2025-07-13 16:47:35 +0300

    login

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,6 @@ from flask_login import login_user
 from app.forms import LoginForm
 from flask_login import login_required, current_user
 from flask_login import login_user, logout_user
-from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -20,15 +19,38 @@ def home():
 @main.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    if form.validate_on_submit():
-#verifica daca este mail existent
-        existing_user_email = User.query.filter_by(email=form.email.data).first()
-        if existing_user_email:
-            flash("Există deja un cont cu acest email.", "warning")
-            return redirect(url_for('main.register'))
+    print("A intrat în ruta /register")
 
+    if form.validate_on_submit():
+        print("Formular validat")
+        print("Email:", form.email.data)
+        print("Parolă:", form.password.data)
+
+        # Hash parolă
+        hashed_pw = generate_password_hash(form.password.data, method='pbkdf2:sha256')
+        print("Parola hashed:", hashed_pw)
+
+        # Creează utilizatorul
+        user = User(email=form.email.data, password=hashed_pw)
+        print("User creat:", user)
+
+        # Adaugă utilizatorul în DB
+        db.session.add(user)
+        try:
+            db.session.commit()
+            print("Contul a fost salvat în DB")
+            flash('Cont creat cu succes!', 'success')
+        except Exception as e:
+            print("Eroare la salvarea în DB:", e)
+
+        return redirect(url_for('main.login'))
+    
+    else:
+        print("Formularul NU a trecut de validare")
+        print("Erori de validare:", form.errors)
 
     return render_template('register.html', form=form)
+
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
@@ -38,15 +60,10 @@ def login():
         if user and check_password_hash(user.password, form.password.data):
             login_user(user)
             flash('Autentificare reușită!', 'success')
-            return redirect(url_for('main.dashboard'))  # sau altă pagină
+            return redirect(url_for('main.home'))  # sau altă pagină
         else:
             flash('Email sau parolă incorecte', 'danger')
     return render_template('login.html', form=form)
 def logout():
     logout_user()
     return redirect(url_for('main.home'))
-
-@main.app_context_processor
-
-def inject_now():
-    return {'now': lambda: datetime.now()}
