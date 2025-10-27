0334cfbc4ca8001114acc037426cffceeb5861d0
iuliann26 <ionita.a.iulian26@gmail.com>
2025-10-27 16:00:54 +0100

    refactor: Load config secrets from environment variables

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'o-cheie-default-nesigura-pt-dev'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
   
