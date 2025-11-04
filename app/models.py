edc64f8d4a00d0072b016f2b84c349d6901d3ed7
iuliann26 <ionita.a.iulian26@gmail.com>
2025-11-04 14:48:57 +0100

    fix(models): Increase password_hash length to 256 for scrypt

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
 
     def set_password(self, password):
